568900a75ed4c5d8af37e24a16165c3a10fea470
pedro-cardoso-a22308918 <pedro_ca_03@outlook.com>
2024-11-30 17:43:42 +0000

    updating the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,15 +23,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer  className={styles.footer}>
+      Pedro Cardoso 2ºAno Lusófona, 2024
+    </footer>
+  )
+}
